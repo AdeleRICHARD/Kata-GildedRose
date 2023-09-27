dad22bc85f25574366b63da35136a16251389d45
AdeleRICHARD <adrichard@figarocms.fr>
2023-09-27 12:07:44 +0200

    Add interface and struct for objects

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,11 +1,100 @@
 package gildedrose
 
-type Item struct {
-	Name            string
-	SellIn, Quality int
+import "github.com/emilybache/gildedrose-refactoring-kata/gildedrose/model"
+
+/* 	- Once the sell by date has passed, Quality degrades twice as fast
+	- The Quality of an item is never negative
+	- "Aged Brie" actually increases in Quality the older it gets
+	- The Quality of an item is never more than 50
+	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	Quality drops to 0 after the concert
+
+We have recently signed a supplier of conjured items. This requires an update to our system:
+
+	- "Conjured" items degrade in Quality twice as fast as normal items
+
+	Operations for all items:
+
+	All items have a SellIn value which denotes the number of days we have to sell the item
+	All items have a Quality value which denotes how valuable the item is
+	At the end of each day our system lowers both values for every item
+	Once the sell by date has passed, Quality degrades twice as fast
+	The Quality of an item is never negative
+	Operations for specific items:
+
+	"Aged Brie" actually increases in Quality the older it gets
+	The Quality of an item is never more than 50
+	"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+	"Conjured" items degrade in Quality twice as fast as normal items
+*/
+
+type GildedRose interface {
+	UpdateQuality()
 }
 
-func UpdateQuality(items []*Item) {
+type GenerallItem struct {
+	item *model.Item
+}
+
+type AgedBrie struct {
+	item *model.Item
+}
+
+type Sulfuras struct {
+	item *model.Item
+}
+
+type BackstagePasses struct {
+	item *model.Item
+}
+
+type Conjured struct {
+	item *model.Item
+}
+
+func NewItems(gildedRoseItems []*model.Item) []GildedRose {
+	items := make([]GildedRose, 0, len(gildedRoseItems))
+	for _, item := range gildedRoseItems {
+		switch item.Name {
+		case "Aged Brie":
+			items = append(items, &AgedBrie{item: item})
+		case "Sulfuras, Hand of Ragnaros":
+			items = append(items, &Sulfuras{item: item})
+		case "Backstage passes to a TAFKAL80ETC concert":
+			items = append(items, &BackstagePasses{item: item})
+		case "Conjured Mana Cake":
+			items = append(items, &Conjured{item: item})
+		default:
+			items = append(items, &GenerallItem{item: item})
+		}
+	}
+	return items
+}
+
+func (gi *GenerallItem) UpdateQuality() {
+	// Will update the quality of the item
+}
+
+func (ab *AgedBrie) UpdateQuality() {
+	// Will update the quality of the item
+}
+
+func (s *Sulfuras) UpdateQuality() {
+	// Will update the quality of the item
+}
+
+func (bp *BackstagePasses) UpdateQuality() {
+	// Will update the quality of the item
+}
+
+func (c *Conjured) UpdateQuality() {
+	// Will update the quality of the item
+}
+
+/* func UpdateQuality(items []*model.Item) {
 	for i := 0; i < len(items); i++ {
 
 		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
@@ -54,5 +143,4 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 	}
-
-}
+} */
--- go/texttest_fixture.go
+++ go/texttest_fixture.go
@@ -6,22 +6,25 @@ import (
 	"strconv"
 
 	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose"
+	"github.com/emilybache/gildedrose-refactoring-kata/gildedrose/model"
 )
 
 func main() {
 	fmt.Println("OMGHAI!")
-
-	var items = []*gildedrose.Item{
-		{"+5 Dexterity Vest", 10, 20},
-		{"Aged Brie", 2, 0},
-		{"Elixir of the Mongoose", 5, 7},
-		{"Sulfuras, Hand of Ragnaros", 0, 80},
-		{"Sulfuras, Hand of Ragnaros", -1, 80},
-		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
-		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
-		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
-		{"Conjured Mana Cake", 3, 6}, // <-- :O
+	var items = []*model.Item{
+		{Name: "+5 Dexterity Vest", SellIn: 10, Quality: 20},
+		{Name: "Aged Brie", SellIn: 2, Quality: 0},
+		{Name: "Elixir of the Mongoose", SellIn: 5, Quality: 7},
+		{Name: "Sulfuras, Hand of Ragnaros", SellIn: 0, Quality: 80},
+		{Name: "Sulfuras, Hand of Ragnaros", SellIn: -1, Quality: 80},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 15, Quality: 20},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 10, Quality: 49},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 5, Quality: 49},
+		// this conjured item does not work properly yet
+		{Name: "Conjured Mana Cake", SellIn: 3, Quality: 6},
 	}
+
+	newGildedRoseItems := gildedrose.NewItems(items)
 
 	days := 2
 	var err error
@@ -37,10 +40,8 @@ func main() {
 	for day := 0; day < days; day++ {
 		fmt.Printf("-------- day %d --------\n", day)
 		fmt.Println("Name, SellIn, Quality")
-		for i := 0; i < len(items); i++ {
-			fmt.Println(items[i])
+		for _, item := range newGildedRoseItems {
+			item.UpdateQuality()
 		}
-		fmt.Println("")
-		gildedrose.UpdateQuality(items)
 	}
 }
